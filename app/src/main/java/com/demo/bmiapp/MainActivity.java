3c42982e5274541f7c80de3f8918a7c75b678486
Nelidel Tingson <nelidel12@gmail.com>
2022-12-03 22:37:33 +0800

    BMIAPP CALCULATOR

---
+++ app/src/main/java/com/demo/bmiapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.demo.bmiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    static String BMIAPP(double weight, double height) {
+
+        double bmi = weight / (height * height);
+
+        if (bmi < 18.5) {
+            return "Underweight";
+        } else if (bmi < 25) {
+            return "Normal";
+        } else if (bmi < 30) {
+            return "Overweight";
+        } else {
+            return "Obese";
+        }
+    }
+}
